34a13a2895a0413e499172bcad670e4dd1d2a9f5
Daniel Palamarchuk <danielpalamarchuk@gmail.com>
2023-10-10 23:33:34 -0400

    First Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const D3Node = require('d3-node');
+const d3 = require('d3');
+
+const http = require('http');
+ 
+const hostname = '127.0.0.1';
+const port = 3000;
+ 
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+ 
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
